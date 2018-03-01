5f926d347726e8c4fabf675a9606a66d704c09b5
Patryk Prusek <prusek.patryk@gmail.com>
2018-03-01 11:55:16 +0100

    Update DataService.php or ClientFactory.php
    Please change scope for setupRestHandler or give possibility to add a custom client in client factory. It would be a helpful for more extensive error logging systems. for example I wanted integrate eightpoints/guzzle-bundle.

--- src/DataService/DataService.php
+++ src/DataService/DataService.php
@@ -201,7 +201,7 @@ class DataService
      * @var ServiceContext $serviceContext         The service Context for this DataService
      *
      */
-    private function setupRestHandler($serviceContext)
+    protected function setupRestHandler($serviceContext)
     {
        if(isset($serviceContext)){
           $client = ClientFactory::createClient($this->getClientName());
