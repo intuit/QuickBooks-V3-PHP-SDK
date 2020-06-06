8ea3cadbaa678c9312582d4aa3e2707b14439d29
Brinda Sivalingam <brinda_sivalingam@intuit.com>
2020-06-05 17:06:04 -0700

    Update SDK user agent to latest release.

--- src/Core/CoreConstants.php
+++ src/Core/CoreConstants.php
@@ -298,7 +298,7 @@ class CoreConstants
      * The Request source header value.
      * @var string REQUESTSOURCEHEADER
      */
-    const USERAGENT = "V3PHPSDK5.3.8";
+    const USERAGENT = "V3PHPSDK5.4.0";
 
     public static function getType($string, $return=1)
     {
