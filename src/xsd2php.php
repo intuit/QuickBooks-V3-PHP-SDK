1aff8fd567ce7f80da116038581885f464ff4cd1
abisalehalliprasan <anil_kumar3@intuit.com>
2020-08-20 13:43:33 -0700

    Adding XSD for minor verison 53

--- src/xsd2php.php
+++ src/xsd2php.php
@@ -3,7 +3,7 @@
 require('./config.php');
 
 use QuickBooksOnline\API\XSD2PHP\src\com\mikebevz\xsd2php\Xsd2Php;
-$xml = new Xsd2Php('IPP', './XSD/Finance.xsd', true);
+$xml = new Xsd2Php('IPP', './XSD/IntuitBaseTypes.xsd', true);
 $xml->overrideAsSingleNamespace = 'http://schema.intuit.com/finance/v3';
 $xml->saveClasses('./Data', true);
 echo "\n";
