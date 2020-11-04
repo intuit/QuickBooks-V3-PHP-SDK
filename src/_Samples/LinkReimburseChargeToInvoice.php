5f7dbbdc27ed8360d0e9704bf272e019ce122de7
abisalehalliprasan <anil_kumar3@intuit.com>
2020-11-03 17:38:27 -0800

    Release Candidate v1947 and hacktober fest PR's

--- src/_Samples/LinkReimburseChargeToInvoice.php
+++ src/_Samples/LinkReimburseChargeToInvoice.php
@@ -6,9 +6,7 @@ use QuickBooksOnline\API\Core\ServiceContext;
 use QuickBooksOnline\API\DataService\DataService;
 use QuickBooksOnline\API\PlatformService\PlatformService;
 use QuickBooksOnline\API\Core\Http\Serialization\XmlObjectSerializer;
-use QuickBooksOnline\API\Facades\Payment;
 use QuickBooksOnline\API\Facades\Invoice;
-
 
 // Prep Data Services
 $dataService = DataService::Configure(array(
@@ -20,14 +18,45 @@ $dataService = DataService::Configure(array(
   'QBORealmID'      => "xxxxxxxxxxxxxxxxxxx",
   'baseUrl'         => "development"
 ));
-
 $dataService->setLogLocation("/Users/hlu2/Desktop/newFolderForLog");
 
 $dataService->throwExceptionOnError(true);
-
-$theResourceObj = $dataService->FindbyId('payment', 11);
-
-$resultingObj = $dataService->void($theResourceObj);
+//Add a new Invoice
+$theResourceObj = Invoice::create([
+   "LinkedTxn" => [
+        "TxnId" => 9,
+        "TxnType" => "ReimburseCharge"
+   ],
+     "Line" => [
+       [
+         "Amount" => 100.00,
+         "DetailType" => "SalesItemLineDetail",
+         "SalesItemLineDetail" => [
+           "ItemRef" => [
+             "value" => 2
+           ],
+           "UnitPrice" => 10
+         ],
+         "LinkedTxn" => [
+             "TxnId" => 9,
+             "TxnType" => "ReimburseCharge"
+         ],
+       ]
+    ],
+"CustomerRef"=> [
+  "value"=> 2
+],
+      "BillEmail" => [
+            "Address" => "Familiystore@intuit.com"
+      ],
+      "BillEmailCc" => [
+            "Address" => "a@intuit.com"
+      ],
+      "BillEmailBcc" => [
+            "Address" => "v@intuit.com"
+      ]
+]);
+$resultingObj = $dataService->Add($theResourceObj);
 
 
 $error = $dataService->getLastError();
@@ -41,29 +70,3 @@ else {
     $xmlBody = XmlObjectSerializer::getPostXmlFromArbitraryEntity($resultingObj, $urlResource);
     echo $xmlBody . "\n";
 }
-
-/*
-Created Customer Id=801. Reconstructed response body:
-
-<?xml version="1.0" encoding="UTF-8"?>
-<ns0:Customer xmlns:ns0="http://schema.intuit.com/finance/v3">
-  <ns0:Id>801</ns0:Id>
-  <ns0:SyncToken>0</ns0:SyncToken>
-  <ns0:MetaData>
-    <ns0:CreateTime>2013-08-05T07:41:45-07:00</ns0:CreateTime>
-    <ns0:LastUpdatedTime>2013-08-05T07:41:45-07:00</ns0:LastUpdatedTime>
-  </ns0:MetaData>
-  <ns0:GivenName>GivenName21574516</ns0:GivenName>
-  <ns0:FullyQualifiedName>GivenName21574516</ns0:FullyQualifiedName>
-  <ns0:CompanyName>CompanyName426009111</ns0:CompanyName>
-  <ns0:DisplayName>GivenName21574516</ns0:DisplayName>
-  <ns0:PrintOnCheckName>CompanyName426009111</ns0:PrintOnCheckName>
-  <ns0:Active>true</ns0:Active>
-  <ns0:Taxable>true</ns0:Taxable>
-  <ns0:Job>false</ns0:Job>
-  <ns0:BillWithParent>false</ns0:BillWithParent>
-  <ns0:Balance>0</ns0:Balance>
-  <ns0:BalanceWithJobs>0</ns0:BalanceWithJobs>
-  <ns0:PreferredDeliveryMethod>Print</ns0:PreferredDeliveryMethod>
-</ns0:Customer>
-*/
